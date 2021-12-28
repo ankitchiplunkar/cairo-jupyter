4ab5067c5b4249e446ff61514fff2c200bf557ea
Ankit Chiplunkar <ankitchiplunkar@gmail.com>
2021-12-28 13:11:30 +0000

    updated readme

--- setup.py
+++ setup.py
@@ -1,15 +1,41 @@
+import os
+import sys
+from setuptools import (
+    find_packages,
+    setup,
+)
+from distutils.command.install import install
+from cairo_kernel import install as kernel_install
+from os import path
 
-from distutils.core import setup
+# managing version
+with open("VERSION", 'r') as f:
+    VERSION = f.read()
 
-with open('README.md') as f:
+# managing readme
+this_directory = path.abspath(path.dirname(__file__))
+with open(path.join(this_directory, 'README.md'), 'r') as f:
     readme = f.read()
+
+class InstallWithKernelspec(install):
+    def run(self):
+        # Regular installation
+        install.run(self)
+
+        # Kernel installation
+        if "NO_KERNEL_INSTALL" in os.environ:
+            # If the NO_KERNEL_INSTALL env variable is set then skip the kernel installation.
+            return
+        else:
+            kernel_install.main(argv=sys.argv)
 
 setup(
     name='cairo_kernel',
-    version='0.0.1',
+    version=VERSION,
     packages=['cairo_kernel'],
-    description='Jupyter kernel for Cairo landuage',
+    description='Jupyter kernel for Cairo language',
     long_description=readme,
+    long_description_content_type='text/markdown',
     author='Ankit Chiplunkar',
     author_email='ankitchiplunkar@gmail.com',
     python_requires='>=3.6, <4',
