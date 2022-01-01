1de9c0c380283ad2088299e2a36105e76398a193
Arie Benhamou <benhamou.arie@gmail.com>
2022-01-01 18:07:13 +0200

    add pytest tests

--- repl_test.py
+++ repl_test.py
@@ -1,19 +1,28 @@
 from cairo_kernel.repl import Repl
+import pytest
 
 my_repl = Repl()
+
+def try_and_except(func):
+    try:
+        my_repl.run(func)
+    except:
+        pytest.fail()
+
+
 func = """
 func add2(x : felt) -> (x : felt): 
-    return (x=x+2) 
+    return (x=x+2) das
     end
 """
-my_repl.run(func)
+try_and_except(func)
 
 multiple_imports = """
 from starkware.cairo.common.uint256 import (
     Uint256, uint256_add, uint256_sub, uint256_lt, uint256_check
 )
 """
-my_repl.run(multiple_imports)
+try_and_except(multiple_imports)
 
 import_struct = """
 struct Location:
@@ -21,4 +30,4 @@ struct Location:
     member col : felt
 end"""
 
-my_repl.run(import_struct)
+try_and_except(import_struct)
