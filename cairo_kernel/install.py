9e4c75a8517302979f050499c6d5c5098b4ce214
Ankit Chiplunkar <ankitchiplunkar@gmail.com>
2022-02-11 13:51:31 +0000

    fix kernel install via pip

--- cairo_kernel/install.py
+++ cairo_kernel/install.py
@@ -1,50 +1,65 @@
-import argparse
-import json
+"""Script that installs the kernel"""
+
 import os
 import sys
+import json
+import getopt
+from shutil import copyfile
 
-from jupyter_client.kernelspec import KernelSpecManager
+try:
+    from jupyter_client.kernelspec import install_kernel_spec
+except ImportError:
+    try:
+        from IPython.kernel.kernelspec import install_kernel_spec
+    except ImportError:
+        print("Please install either Jupyter to IPython before continuing")
 from IPython.utils.tempdir import TemporaryDirectory
 
 kernel_json = {
-    "argv": [sys.executable, "-m", "cairo_kernel", "-f", "{connection_file}"],
+    "argv": [sys.executable,
+             "-m", "cairo_kernel", "-f", "{connection_file}"],
     "display_name": "Cairo",
     "language": "text",
+    "name": "Cairo",
 }
 
+
 def install_my_kernel_spec(user=True, prefix=None):
+    user = '--user' in sys.argv or not _is_root()
     with TemporaryDirectory() as td:
-        os.chmod(td, 0o755) # Starts off as 700, not user readable
+        os.chmod(td, 0o755)  # Starts off as 700, not user readable
+
         with open(os.path.join(td, 'kernel.json'), 'w') as f:
             json.dump(kernel_json, f, sort_keys=True)
-        # TODO: Copy any resources
 
-        print('Installing Jupyter kernel spec')
-        KernelSpecManager().install_kernel_spec(td, 'cairo', user=user, replace=True, prefix=prefix)
+        try:
+            install_kernel_spec(td, kernel_json['name'], user=user,
+                                replace=True, prefix=prefix)
+        except OSError:
+            install_kernel_spec(td, kernel_json['name'], user=not user,
+                                replace=True, prefix=prefix)
+
+def main(argv=()):
+    prefix = None
+    user = not _is_root()
+
+    opts, _ = getopt.getopt(argv[1:], '', ['user', 'prefix='])
+    for k, v in opts:
+        if k == '--user':
+            user = True
+        elif k == '--prefix':
+            prefix = v
+            user = False
+
+    install_my_kernel_spec(user=user, prefix=prefix)
+
 
 def _is_root():
     try:
         return os.geteuid() == 0
     except AttributeError:
-        return False # assume not an admin on non-Unix platforms
+        return False  # assume not an admin on non-Unix platforms
 
-def main(argv=None):
-    ap = argparse.ArgumentParser()
-    ap.add_argument('--user', action='store_true',
-        help="Install to the per-user kernels registry. Default if not root.")
-    ap.add_argument('--sys-prefix', action='store_true',
-        help="Install to sys.prefix (e.g. a virtualenv or conda env)")
-    ap.add_argument('--prefix',
-        help="Install to the given prefix. "
-             "Kernelspec will be installed in {PREFIX}/share/jupyter/kernels/")
-    args = ap.parse_args(argv)
-
-    if args.sys_prefix:
-        args.prefix = sys.prefix
-    if not args.prefix and not _is_root():
-        args.user = True
-
-    install_my_kernel_spec(user=args.user, prefix=args.prefix)
 
 if __name__ == '__main__':
-    main()
+    main(argv=sys.argv)
